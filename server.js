6f3c6a4b92952f34919e3cf4febb2f755a0f2772
abanoub.arnest <abanoub.arnest@remindermedia.com>
2024-10-09 11:58:15 +0300

    start server express

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
 // # Entry point to start the server
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Initial test endpoint
+app.get('/', (req, res) => {
+    res.send('Server is up and running!');
+});
+
+// Start server
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
